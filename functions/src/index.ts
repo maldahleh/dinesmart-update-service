caf06cc04892b207f73fdd2f3bc4d9e1007b7883
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-02-03 18:50:42 -0500

    Adds Toronto inspection updater

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,6 @@
 import * as functions from "firebase-functions";
+import updateTorontoInspections from './inspections/torontoInspections';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const main = functions.https.onRequest((_request, _response) => {
+    updateTorontoInspections();
+});
---
+++ functions/src/inspections/torontoInspections.ts
@@ -0,0 +1,74 @@
+import { parseString } from 'xml2js';
+import addToStorage from '../storage/storageService';
+import xmlDownloader from '../utils/xmlDownloader';
+
+const torontoDinesafeUrl = 'http://opendata.toronto.ca/public.health/dinesafe/dinesafe.zip';
+
+export default () => {
+    xmlDownloader(torontoDinesafeUrl, (text: string) => {
+        if (!text) {
+            return;
+        }
+
+        parseString(text, (err, result) => {
+            if (err) {
+                return;
+            }
+
+            const inspections = {};
+            result['ROWDATA']['ROW'].forEach(res => {
+                let existingData = inspections[res['ESTABLISHMENT_ID'][0]];
+                if (typeof existingData === 'undefined') {
+                    existingData = {
+                        'name': res['ESTABLISHMENT_NAME'][0].trim(),
+                        'type': res['ESTABLISHMENTTYPE'][0],
+                        'address': res['ESTABLISHMENT_ADDRESS'][0].trim(),
+                        'coords': {
+                            'lat': res['LATITUDE'][0],
+                            'lon': res['LONGITUDE'][0]
+                        },
+                        'inspections': {
+                        }
+                    };
+                }
+
+                let inspectionData = existingData['inspections'][res['INSPECTION_ID'][0]];
+                if (typeof inspectionData === 'undefined') {
+                    inspectionData = {
+                        'date': res['INSPECTION_DATE'][0],
+                        'status': res['ESTABLISHMENT_STATUS'][0],
+                        'infractions': []
+                    }
+                }
+
+                const infractionDetails = res['INFRACTION_DETAILS'][0];
+                if (infractionDetails !== '') {
+                    let severity = res['SEVERITY'][0];
+                    if (severity.startsWith('NA')) {
+                        severity = severity.substring(5);
+                    } else {
+                        severity = severity.substring(4);
+                    }
+
+                    inspectionData['infractions'].push({
+                        'details': infractionDetails,
+                        'severity': severity
+                    });
+                }
+
+                existingData['inspections'][res['INSPECTION_ID'][0]] = inspectionData;
+                inspections[res['ESTABLISHMENT_ID'][0]] = existingData;
+            });
+
+            Object.keys(inspections).forEach(inspection => {
+                let inspectionArray = Object.values(inspections[inspection]['inspections']);
+                inspectionArray.sort((a, b) => (b['date'] > a['date']) ? 1
+                    : ((a['date'] > b['date']) ? -1 : 0));
+
+                inspections[inspection]['inspections'] = inspectionArray;
+
+                addToStorage(inspection, inspections[inspection]);
+            });
+        });
+    });
+};
