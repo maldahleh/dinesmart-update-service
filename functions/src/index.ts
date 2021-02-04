c25ad9c8fa452234783ecaefa90f775e21d4afbf
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-02-04 10:34:17 -0500

    Adds promise support at index.ts layer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,12 @@
 import * as functions from "firebase-functions";
 import updateTorontoInspections from "./inspections/torontoInspections";
 
-export const main = functions.https.onRequest((_request, response) => {
-  updateTorontoInspections((res) => {
-    if (res) {
-      response.send("Toronto: Inspection data updated");
-    } else {
-      response.send("Toronto: Inspection data update failed");
-    }
-  });
+export const main = functions.https.onRequest(async (_request, response) => {
+  await updateTorontoInspections()
+      .then(() => {
+        response.send("Toronto: Inspection data updated");
+      })
+      .catch((err) => {
+        response.send(`Toronto: Inspection data update failed. err=${err}`);
+      });
 });
