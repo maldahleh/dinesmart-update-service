e14ee022d7beaa5934f1d543e1707dc08d962075
Mohammed Al-Dahleh <maldahleh@gmail.com>
2024-01-05 12:25:06 -0500

    Fixes imports

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,12 @@
 // eslint-disable-next-line import/no-unresolved
 import {onSchedule} from "firebase-functions/v2/scheduler";
-import {logger} from "firebase-functions";
+// eslint-disable-next-line import/no-unresolved
+import {log, error} from "firebase-functions/logger";
 import updateTorontoInspections from "./inspections/toronto/torontoInspections";
 
 exports.updateData = onSchedule("every day 00:00", async () => {
   await updateTorontoInspections()
-      .catch((err) => logger.error(`Update failed. err=${err}`));
+      .catch((err) => error(`Update failed. err=${err}`));
 
-  logger.log("Updated inspection data");
+  log("Updated inspection data");
 });
