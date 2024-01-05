b669197d176b567dfc719344a02c7503cacf1792
Mohammed Al-Dahleh <maldahleh@gmail.com>
2024-01-04 23:06:20 -0500

    Simplifies code logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,7 @@ import updateTorontoInspections from "./inspections/toronto/torontoInspections";
 
 exports.updateData = onSchedule("every day 00:00", async () => {
   await updateTorontoInspections()
-      // eslint-disable-next-line max-len
-      .catch((err) => logger.error(`Inspection data update failed. err=${err}`));
+      .catch((err) => logger.error(`Update failed. err=${err}`));
 
   logger.log("Updated inspection data");
 });
